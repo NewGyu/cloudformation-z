765422ce5ebc0bb226358533e41b1e164761d9e2
NewGyu <NewGyu@kinoboku.net>
2015-12-18 17:40:12 +0900

    COMPLETEするまでstack eventsをpollingするようにした。

--- index.js
+++ index.js
@@ -5,20 +5,3 @@ var commander = require("commander");
 
 module.exports.Deployer = require("./lib/deployer");
 module.exports.EasyCommander = require("./lib/easyCommander");
-
-/**
- * @return {Promise}
- */
-module.exports.deploy = function(config, cfnTemplate) {
-  var deployer = new Deployer(config);
-  return deployer.deploy(cfnTemplate);
-}
-
-
-/**
- * @return {Promise}
- */
-module.exports.validate = function(config, cfnTemplate) {
-  var deployer = new Deployer(config);
-  return deployer.validate(cfnTemplate);
-}
